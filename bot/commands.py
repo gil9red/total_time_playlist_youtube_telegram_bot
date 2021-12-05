f3fbe0f3e46a4d06fa5cc284074747cac6cfe1c4
ipetrash <ilya.petrash@inbox.ru>
2021-12-06 00:40:33 +0500

    Updated. Added error logging for incorrect playlist ID or URL (only for admin)

--- bot/commands.py
+++ bot/commands.py
@@ -41,7 +41,9 @@ def reply_playlist(playlist_id_or_url: str, update: Update, context: CallbackCon
         playlist = Playlist.get_from(playlist_id_or_url)
     except:
         text = 'Invalid playlist id or url!'
-        log.exception(text)
+
+        if is_admin(update):
+            log.exception(text)
 
         reply_message(text, update, context, severity=SeverityEnum.ERROR)
         return
